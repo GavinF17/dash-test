f50c172919e7f94c5fe04853c89156de647d3af2
Gavin Fenton <gavin_fenton@msn.com>
2021-05-14 23:55:15 +0100

    initial

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,37 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {useHistory} from 'react-router-dom';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const {push} = useHistory()
+
+    const ghurl = "https://github.com/login/oauth/authorize" +
+        "?client_id=8117784c80d8d9ea3b5d" +
+        "&redirect_uri=https://dash-test.gavinfenton.com/test" +
+        "&scope=repo,repo_deployment,notifications,workflow"
+
+    const tryThis = () => fetch(ghurl, {mode: 'no-cors', redirect: 'follow'})
+        .then(response => {
+            console.log(response)
+            push(response.url)
+        })
+        .catch(console.error)
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
+                <p>
+                    Edit <code>src/App.tsx</code> and save to reload.
+                </p>
+
+                <a href="https://github.com/login/oauth/authorize">github</a>
+
+                <button onClick={tryThis}>try</button>
+            </header>
+        </div>
+    );
 }
 
 export default App;
